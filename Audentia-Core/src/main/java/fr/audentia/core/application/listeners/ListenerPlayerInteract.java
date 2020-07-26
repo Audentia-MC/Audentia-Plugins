1a7db1a143ad9535c823c8d7fb311dfd6f00017e
Emalios <hugo.bergerat2002@gmail.com>
2020-07-26 22:03:27 +0200

    Refactor conditions in listeners

--- Audentia-Core/src/main/java/fr/audentia/core/application/listeners/ListenerPlayerInteract.java
+++ Audentia-Core/src/main/java/fr/audentia/core/application/listeners/ListenerPlayerInteract.java
@@ -18,7 +18,7 @@ public class ListenerPlayerInteract implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
 
-        if(!(this.gamesInfosRepository.getGameState() == GameState.PENDANT)) {
+        if(this.gamesInfosRepository.getGameState() != GameState.PENDANT) {
             event.setUseItemInHand(Event.Result.DENY);
             event.setUseInteractedBlock(Event.Result.DENY);
             event.setCancelled(true);
