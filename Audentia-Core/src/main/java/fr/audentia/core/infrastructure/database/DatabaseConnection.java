6ea8fa2ff7574ff2e65474ec79fbabc9090e3752
LukaMrt <maret.luka@gmail.com>
2020-08-02 19:00:23 +0200

    add database connection

---
+++ Audentia-Core/src/main/java/fr/audentia/core/infrastructure/database/DatabaseConnection.java
@@ -0,0 +1,37 @@
+package fr.audentia.core.infrastructure.database;
+
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private final HikariDataSource dataSource;
+
+    public DatabaseConnection(DatabaseConnectionBuilder builder) {
+        dataSource = new HikariDataSource();
+        setUpDataSource(builder);
+    }
+
+    private void setUpDataSource(DatabaseConnectionBuilder builder) {
+        dataSource.setJdbcUrl("jdbc:mysql://" + builder.getHost() + ":3306/" + builder.getDatabase());
+        dataSource.setUsername(builder.getUser());
+        dataSource.setPassword(builder.getPassword());
+        dataSource.addDataSourceProperty("autoReconnect", true);
+        dataSource.addDataSourceProperty("tcpKeepAlive", true);
+        dataSource.addDataSourceProperty("serverTimezone", "Europe/Paris");
+        dataSource.addDataSourceProperty("characterEncoding", "utf8");
+        dataSource.addDataSourceProperty("useUnicode", "true");
+        dataSource.setMaximumPoolSize(15);
+        dataSource.setMinimumIdle(0);
+    }
+
+    public DSLContext getDatabaseContext() throws SQLException {
+        return DSL.using(dataSource.getConnection(), SQLDialect.MARIADB);
+    }
+
+}
---
+++ Audentia-Core/src/main/java/fr/audentia/core/infrastructure/database/DatabaseConnectionBuilder.java
@@ -0,0 +1,56 @@
+package fr.audentia.core.infrastructure.database;
+
+public class DatabaseConnectionBuilder {
+
+    private String host;
+    private String user;
+    private String password;
+    private String database;
+
+    private DatabaseConnectionBuilder() {
+    }
+
+    public static DatabaseConnectionBuilder aDatabaseConnection() {
+        return new DatabaseConnectionBuilder();
+    }
+
+    public DatabaseConnectionBuilder withHost(String host) {
+        this.host = host;
+        return this;
+    }
+
+    public DatabaseConnectionBuilder withUser(String user) {
+        this.user = user;
+        return this;
+    }
+
+    public DatabaseConnectionBuilder withPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public DatabaseConnectionBuilder withDatabase(String database) {
+        this.database = database;
+        return this;
+    }
+
+    public DatabaseConnection build() {
+        return new DatabaseConnection(this);
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+}
